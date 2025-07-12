19049880136ceb5d2a6951946df50787b74b15d9
Bobby <bobbyskhs@gmail.com>
2025-07-12 16:04:49 +0530

    not found controller

--- router/routes.go
+++ router/routes.go
@@ -18,11 +18,5 @@ func Initialize(router *fiber.App) {
 	// router.Get("/login", controllers.LoginController)
 	// router.Get("/preferences", controllers.PreferencesController)
 
-	router.Use(func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error":   "Not Found",
-			"message": "The requested resource could not be found.",
-			"status":  fiber.StatusNotFound,
-		})
-	})
+	router.Use(controllers.NotFoundController)
 }
